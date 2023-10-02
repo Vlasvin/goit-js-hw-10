0cc131ad144214ad9618fcd5f9ad4468343dcdcd
Oleksii <103183090+Vlasvin@users.noreply.github.com>
2023-10-02 17:17:45 +0300

    created_main_cod

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,4 +1,3 @@
-import SlimSelect from 'slim-select';
 import axios from 'axios';
 
 const BASE_URL = `https://api.thecatapi.com/v1/`;
@@ -6,20 +5,15 @@ axios.defaults.headers.common['x-api-key'] =
   'live_icm1GCVwjCYsiZDr2jieZaw0CYZp62lKBeAX40dJr4XgKQxo0FhDnwQaWbzMWYpI';
 
 function fetchBreeds() {
-  return axios
-    .get(`${BASE_URL}breeds`)
-    .then(response => {
-      return response.data;
-    })
-    .catch(response => {
-      console.log(Error);
-    });
+  return axios.get(`${BASE_URL}breeds`).then(response => {
+    return response.data;
+  });
 }
+
 function fetchCatByBreed(breedId) {
   return axios
     .get(`${BASE_URL}images/search?breed_ids=${breedId}`)
     .then(response => {
-      // console.log(response.data);
       return response.data;
     });
 }
--- src/index.js
+++ src/index.js
@@ -7,27 +7,35 @@ import { fetchCatByBreed } from './cat-api';
 axios.defaults.headers.common['x-api-key'] =
   'live_icm1GCVwjCYsiZDr2jieZaw0CYZp62lKBeAX40dJr4XgKQxo0FhDnwQaWbzMWYpI';
 
-let breedSelect = document.querySelector('.breed-select');
+const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const error = document.querySelector('.error');
 const loader = document.querySelector('.loader');
 
 loader.textContent = '';
-
+error.textContent = '';
+offSelect();
 createBreeds();
 
 function createBreeds() {
-  fetchBreeds().then(data => {
-    let selectData = data.map(item => ({
-      text: item.name,
-      value: item.id,
-    }));
-    const select = new SlimSelect({
-      select: '.breed-select',
-      data: selectData,
+  fetchBreeds()
+    .then(data => {
+      let selectData = data.map(item => ({
+        text: item.name,
+        value: item.id,
+      }));
+      onSelect();
+      const select = new SlimSelect({
+        select: '.breed-select',
+        data: selectData,
+      });
+      selectBreed();
+      offLoader();
+    })
+    .catch(data => {
+      offLoader();
+      onError();
     });
-    selectBreed();
-  });
 }
 
 function selectBreed() {
@@ -35,21 +43,67 @@ function selectBreed() {
     select: '.breed-select',
     events: {
       afterChange: newVal => {
-        fetchCatByBreed(newVal[0].value);
+        offContent();
+        onLoader();
+        fetchCatByBreed(newVal[0].value)
+          .then(response => {
+            addContent(response);
+          })
+          .catch(response => {
+            offSelect();
+            offLoader();
+            onError();
+          });
       },
     },
   });
 }
 
-function addContent() {
-  new SlimSelect({
-    select: '.breed-select',
-    events: {
-      afterClose: () => {
-        fetchCatByBreed().then(response => {
-          console.log(response);
-        });
-      },
-    },
+function addContent(response) {
+  response.forEach(cat => {
+    const name = cat.breeds[0].name;
+    const description = cat.breeds[0].description;
+    const temperament = cat.breeds[0].temperament;
+    const imageUrl = cat.url;
+
+    catInfo.innerHTML = `
+    <img src="${imageUrl}" alt="Cat Photo">
+    <div>
+     <h2>${name}</h2>
+      <p>${description}</p>
+      <p>Temperament: ${temperament}</p></div>
+    `;
+    catInfo.style.cssText = 'display: flex; gap:40px';
+    catInfo.querySelector('div').classList.add('content-text');
+    catInfo.querySelector('img').style.cssText = 'width: 700px; height: 600px;';
+    offLoader();
+    onContent();
   });
+}
+function offLoader() {
+  loader.style.display = 'none';
+}
+function onLoader() {
+  loader.style.display = 'inline-block';
+}
+function offContent() {
+  catInfo.style.display = 'none';
+}
+function onContent() {
+  catInfo.style.display = 'flex';
+}
+function onError() {
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page!',
+    {
+      position: 'left-top',
+      timeout: 20000,
+    }
+  );
+}
+function offSelect() {
+  breedSelect.style.display = 'none';
+}
+function onSelect() {
+  breedSelect.style.display = 'flex';
 }
